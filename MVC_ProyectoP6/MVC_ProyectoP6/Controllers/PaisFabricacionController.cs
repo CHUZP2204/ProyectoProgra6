a168af0c6b80b7c88a0f11d73b507a4eaace81e5
WAGNER DANILO DIAZ DIAZ <116180080@castrocarazo.ac.cr>
2021-02-17 10:24:00 -0600

    controller Pais fabricacion y su vista

---
+++ MVC_ProyectoP6/MVC_ProyectoP6/Controllers/PaisFabricacionController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_ProyectoP6.Controllers
+{
+    public class PaisFabricacionController : Controller
+    {
+        // GET: Registro PaisFabricacion
+        //
+        public ActionResult InsertaPaisFabricacion()
+        {
+            return View();
+        }
+    }
+}

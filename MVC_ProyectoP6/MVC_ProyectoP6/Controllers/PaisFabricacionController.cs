16a720213a5ab3b5050d5657fc9d1e55a700bf99
WAGNER DANILO DIAZ DIAZ <116180080@castrocarazo.ac.cr>
2021-02-18 12:41:42 -0600

    lista y modifica pais fabricacion

--- MVC_ProyectoP6/MVC_ProyectoP6/Controllers/PaisFabricacionController.cs
+++ MVC_ProyectoP6/MVC_ProyectoP6/Controllers/PaisFabricacionController.cs
@@ -34,7 +34,7 @@ namespace MVC_ProyectoP6.Controllers
             catch (Exception error)
             {
                 resultado = "Ocurrio un error: " + error.Message;
-                
+
             }
             finally
             {
@@ -44,11 +44,71 @@ namespace MVC_ProyectoP6.Controllers
                 }
                 else
                 {
-                    resultado += "No se pudo Insertar";            
+                    resultado += "No se pudo Insertar";
                 }
             }
             Response.Write("<script languaje=javascript>alert('" + resultado + "');</script>");
             return View();
         }
+        public ActionResult ListaPaisFabricante()
+        {
+            ///crear la variable que contiene el registro obtenidos
+            ///mediante invocar al procedimiento almacenados 
+            List<sp_RetornaPaisFabricante_Result> modeloVista =
+                new List<sp_RetornaPaisFabricante_Result>();
+
+            //asignar a la variable el resultado de llamar al SP
+            modeloVista = this.modeloBD.sp_RetornaPaisFabricante("", "").ToList();
+
+            //enciar el modelo a la vista
+            return View(modeloVista);
+        }
+        public ActionResult ModificaPaisFabricante(int idPaisFabricante)
+        {    ///obtener el registro que se desea modificar
+             /// utilizando el parametro del metodo idPaisFabricante
+            sp_RetornaPaisFabricante_ID_Result modeloVista = new sp_RetornaPaisFabricante_ID_Result();
+            modeloVista = this.modeloBD.sp_RetornaPaisFabricante_ID(idPaisFabricante).FirstOrDefault();
+
+            //enviar modelo a la vista
+            return View(modeloVista);
+        }
+        [HttpPost]
+        public ActionResult ModificaPaisFabricante(sp_RetornaPaisFabricante_ID_Result modeloVista)
+        {
+            ///varable que registra la cantidad de registro afectados
+            ///si un SP que se ejecute Insert,UPDATE,DELETE
+            ///no afecta registros implica que hubo un error 
+            int cantidadRegistrosAfectados = 0;
+            string resultado = " ";
+
+            try
+            {
+                cantidadRegistrosAfectados =
+                       this.modeloBD.sp_ModificaPaisFabricante(
+                           modeloVista.idPaisFabricante,
+                           modeloVista.PaisFabricante,
+                           modeloVista.CodigoPaisFabricante
+                           );
+            }
+            catch (Exception error)
+            {
+                resultado = "Ocurrio un Error" + error.Message;
+               
+            }
+            finally
+            {
+                if (cantidadRegistrosAfectados > 0)
+                {
+                    resultado = "El Registro Modificado";
+                }
+                else
+                {
+                    resultado = "No se pudo Modifcar";
+                }
+            }
+            Response.Write("<script languaje=javascript>alert('" + resultado + "');</script>");
+            return View(modeloVista);
+        }
+
     }
 }
---
+++ MVC_ProyectoP6/MVC_ProyectoP6/Scripts/PaisFabricacion/ModificaPaisFabricante.js
@@ -0,0 +1,20 @@
+﻿///document on ready del view Registro de Personas
+$(function () {
+    creaValidaciones();
+});
+
+///crea las validaciones para el formulario
+function creaValidaciones() {
+    $("#frmModificaPaisFabricante").validate({
+        ///objeto que contiene "las condiciones" que el formulario
+        ///debe cumplir para ser considerado válido
+        rules: {
+            PaisFabricante: {
+                required: true
+            },
+            CodigoPaisFabricante: {
+                required: true
+            },
+        }
+    });
+}

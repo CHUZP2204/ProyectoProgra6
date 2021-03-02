07b0495886e95024a3e3cbfa20b98bcb9d4f9756
WAGNER DANILO DIAZ DIAZ <116180080@castrocarazo.ac.cr>
2021-03-01 22:19:51 -0600

    LISTA Inserta SOP

---
+++ MVC_ProyectoP6/MVC_ProyectoP6/Controllers/ServicioOProductoController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_ProyectoP6.Models;
+
+namespace MVC_ProyectoP6.Controllers
+{
+    public class ServicioOProductoController : Controller
+    { 
+        progra6Entities3 modeloBD = new progra6Entities3();
+    
+        // GET: ServicioOProducto
+        public ActionResult ListaServicioOProducto()
+        {
+            ///crear la variable que contiene el registro obtenidos
+            ///mediante invocar al procedimiento almacenados 
+            List<sp_RetornaServicioOProducto_Result> modeloVista =  new List<sp_RetornaServicioOProducto_Result>();
+
+            //asignar a la variable el resultado de llamar al SP
+            modeloVista = this.modeloBD.sp_RetornaServicioOProducto("", "").ToList();
+
+            //enciar el modelo a la vista
+            return View(modeloVista);
+        }
+
+        public ActionResult NuevoServicioOproducto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NuevoServicioOproducto(sp_RetornaServicioOProducto_Result modeloVista)
+        {
+            int cantidadRegistrosAfectados = 0;
+            string resultado = " ";
+            try
+            {
+                cantidadRegistrosAfectados =
+                    this.modeloBD.sp_InsertaServicioOProducto(
+                        modeloVista.CodigoSOP,
+                        modeloVista.PrecioSOP.ToString(),
+                        modeloVista.TipoSOP,
+                        modeloVista.idCliente
+                        );
+            }
+            catch (Exception error)
+            {
+                resultado = "Ocurrio un error: " + error.Message;
+
+            }
+            finally
+            {
+                if (cantidadRegistrosAfectados > 0)
+                {
+                    resultado = "Registro Insertado";
+                }
+                else
+                {
+                    resultado += "No se pudo Insertar";
+                }
+            }
+            Response.Write("<script languaje=javascript>alert('" + resultado + "');</script>");
+            return View();
+        }
+    }
+}
---
+++ MVC_ProyectoP6/MVC_ProyectoP6/Scripts/ServicioProducto/ValidacionServicioProducto.js
@@ -0,0 +1,27 @@
+﻿///document on ready del view Registro de Personas
+$(function () {
+    creaValidaciones();
+});
+
+///crea las validaciones para el formulario
+function creaValidaciones() {
+    $("#frmServicioOProducto").validate({
+        ///objeto que contiene "las condiciones" que el formulario
+        ///debe cumplir para ser considerado válido
+        rules: {
+            CodigoSOP: {
+                required: true
+            },
+            PrecioSOP: {
+                required: true
+            },
+            TipoSOP: {
+                required: true
+            },
+            idCliente: {
+                required: true
+            },
+
+        }
+    });
+}

f8175d7989ee5330c287e2dfb58f4d2ca56b1984
Jesús Saenz <jesussaenz04@gmail.com>
2021-03-11 23:10:38 -0600

    Comentarios Inicio Session

--- MVC_ProyectoP6/MVC_ProyectoP6/Controllers/HomeController.cs
+++ MVC_ProyectoP6/MVC_ProyectoP6/Controllers/HomeController.cs
@@ -14,7 +14,9 @@ namespace MVC_ProyectoP6.Controllers
         {
             return View();
         }
-        /*[HttpPost]
+        /*[HttpPost] 
+         * Metodo Del Lado Del Servidor 
+         * Ignorar Si Se Usa JQUERY
         public ActionResult Index(sp_RetornaClientes_Result modeloVista)
         {
            string contraseniaObtenida = modeloVista.Contrasenia;
@@ -44,14 +46,22 @@ namespace MVC_ProyectoP6.Controllers
             return View();
         }*/
 
+        /// <summary>
+        /// Metodo Que valida El Usuario Que Desee Inicar Session
+        /// Devuelve True o False 
+        /// </summary>
+        /// <param name="pCorreo"></param>
+        /// <param name="pContrasenia"></param>
+        /// <returns></returns>
         public ActionResult ValidarUsuario(string pCorreo, string pContrasenia)
         {
             List<sp_RetornaClientes_Result> listaClientes = new List<sp_RetornaClientes_Result>();
-
             listaClientes = this.ModeloBD.sp_RetornaClientes(null, null).ToList();
 
             bool UsuarioVerificado = false;
 
+            ///Recorrer Lista De Usuarios Registrados
+            ///True Si se Cumple La Contrasenia y Correo A La Vez
             for (int i = 0; i < listaClientes.Count; i++)
             {
                 if (listaClientes[i].Correo.Equals(pCorreo) && listaClientes[i].Contrasenia.Equals(pContrasenia))
@@ -64,12 +74,19 @@ namespace MVC_ProyectoP6.Controllers
                 }
             }
             
+            ///Retornar En Json Para Trabajarlo
+            ///Del Lado Del Cliente
             return Json(new
             {
                 resultado = UsuarioVerificado
             });
         }
 
+        /// <summary>
+        /// Metodo Que Obtiene Informacion Del Cliente Actual Que Inicio Session
+        /// Se Devuelve En Json Para Su Posterior Uso Del Lado Del Cliente
+        /// </summary>
+        /// <returns></returns>
         public ActionResult MostrarInfoUsuario()
         {
             int idUsuarioLogueado = Convert.ToInt32(this.Session["idClienteLoguedo"]);
